158b949d64a18a44d3b91e56d411cfba3e517503
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2025-03-24 01:07:21 +0900

    update entery point : import global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@
 import { render } from 'solid-js/web'
 import App from './App/App.tsx'
 
+import "Global/styles/style.scss";
+
 const root = document.getElementById('root')
 
 render(() => <App />, root!)
