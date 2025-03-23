6c1bbc455430746b6adbe9c9e94fc9fdc34f8d04
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2025-03-24 01:08:49 +0900

    add utilities : loop range and ibw calc func

---
+++ src/global/utils/calcIBW/calcIBW.ts
@@ -0,0 +1,4 @@
+﻿import {Sex} from "Global/enums/Sex.ts";
+
+export default (sex:Sex, height:number) =>
+    (sex==Sex.MALE ? 50:45.5) + (0.91*(height- 152.4))
---
+++ src/global/utils/range/range.ts
@@ -0,0 +1,7 @@
+﻿function* range(end:number){
+    for (let i = 0; i < end; i++){
+        yield i;
+    }
+}
+
+export default range;
