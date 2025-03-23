7c442d6e97dc8fa2a9b3400e03d9304ba1c9bac9
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2025-03-24 00:53:47 +0900

    update config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  resolve: {
+    alias: {
+      "Global/": "/src/global/",
+      "Pages/": "/src/pages/",
+      "Assets/": "/src/assets/",
+      "@/": "/src/",
+      "Shared": "/shared/"
+    }
+  }
 })
